7d4d9cf01977be9e09c681e3233060ef98585b6f
Student <example@example.com>
2019-08-23 11:09:49 -0700

    save 2nd section of JavaScript for dragon input id to duplicate and fill out later

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,11 @@ $(document).ready(function() {
   getInputs("dr", 3);
 
 });
+
+function getInputs(inputId, listCount){
+  $("#form-" + inputId).submit(function(event){
+    event.preventDefault();
+
+    var
+  })
+}
